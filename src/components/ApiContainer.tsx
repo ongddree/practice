ba2e61bab393e2104e7030416d9645a65c45ce60
ongddree <ongddree@gmail.com>
2022-04-06 21:31:01 +0900

    ✨ ApiContainer 생성

---
+++ src/components/ApiContainer.tsx
@@ -0,0 +1,12 @@
+import VideoCard from './VideoCard';
+
+const ApiContainer = () => {
+  return (
+    <section>
+      <h2>코딩 영상 모음</h2>
+      <VideoCard />
+    </section>
+  );
+};
+
+export default ApiContainer;
