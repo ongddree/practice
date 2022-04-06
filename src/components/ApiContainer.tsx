6cc5b7ae12fabff36571516c5a96ed161f1d4319
ongddree <ongddree@gmail.com>
2022-04-06 22:34:33 +0900

    ✨ CodingResources api 연동

--- src/components/ApiContainer.tsx
+++ src/components/ApiContainer.tsx
@@ -1,10 +1,41 @@
+import { useState, useEffect } from 'react';
+import { CodingResourcesType } from '../@types/CodingResources';
+import { API_ENDPOINT } from '../constant';
 import VideoCard from './VideoCard';
 
 const ApiContainer = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
+  const handleData = async () => {
+    await fetch(`${API_ENDPOINT}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <section>
       <h2>코딩 영상 모음</h2>
-      <VideoCard />
+      <ul>
+        {data
+          ?.filter(
+            (e: CodingResourcesType) =>
+              e.topics.includes('js') || e.topics.includes('javascript')
+          )
+          .map((e: CodingResourcesType, i) => {
+            return <VideoCard key={`video-${i}`} videoData={e} />;
+          })}
+      </ul>
     </section>
   );
 };
