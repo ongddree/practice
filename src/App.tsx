a2db7225915c8827e3a7f14966d8ab2feed0c07e
ongddree <ongddree@gmail.com>
2022-04-08 23:26:17 +0900

    ✨ MemoPage 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CodingApiPage from './pages/CodingApiPage';
+import { CodingApiPage, MemoPage } from './pages';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/codingApi" element={<CodingApiPage />} />
+          <Route path="/memo" element={<MemoPage />} />
         </Routes>
       </BrowserRouter>
     </div>
