35667b8193b7124e22177d4a3ae83f8e220d115d
ongddree <ongddree@gmail.com>
2022-04-08 22:20:59 +0900

    ✨ CreateUser 생성

---
+++ src/components/react/CreateUser.tsx
@@ -0,0 +1,28 @@
+interface CreateUserProps {
+  username: string;
+  email: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onCreate: () => void;
+}
+
+function CreateUser({ username, email, onChange, onCreate }: CreateUserProps) {
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+}
+
+export default CreateUser;
