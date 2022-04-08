8eed968dd6350729b0a416966ba0a0c51c53f834
ongddree <ongddree@gmail.com>
2022-04-08 23:25:44 +0900

    ♻️ index 파일 만들어서 묶어서 export

---
+++ src/components/react/index.ts
@@ -0,0 +1,6 @@
+import Counter from './Counter';
+import CreateUser from './CreateUser';
+import InputSample from './InputSample';
+import UserList from './UserList';
+
+export { Counter, CreateUser, InputSample, UserList };
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import CodingApiPage from './CodingApiPage';
+import MemoPage from './MemoPage';
+
+export { CodingApiPage, MemoPage };
