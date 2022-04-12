ff9742e1374e7f9e7dee5a57f6e5e5ece5e4d8ab
ongddree <ongddree@gmail.com>
2022-04-12 14:57:44 +0900

    🏷️ UserInterface, StateInterface, Action 타입 추가

--- src/@types/interface.ts
+++ src/@types/interface.ts
@@ -1,1 +1,21 @@
-export * from './CodingResources';
+interface UserInterface {
+  id: number;
+  username: string;
+  email: string;
+  active: boolean;
+}
+
+interface StateInterface {
+  inputs: {
+    username: string;
+    email: string;
+  };
+  users: UserInterface[];
+}
+
+interface Action {
+  type: string;
+  payload: any;
+}
+
+export type { UserInterface, StateInterface, Action };
