bed379e86640cfcf19d369fb95a21c8ebd6e6d9c
ongddree <ongddree@gmail.com>
2022-05-01 18:09:54 +0900

    ♻️  spacelog api endpoint 추상화

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,5 @@
 export const API_ENDPOINT =
   'https://api.sampleapis.com/codingresources/codingResources';
+
+export const SPACELOG_API_ENDPOINT =
+  'https://secret-hollows-17182.herokuapp.com/api';
