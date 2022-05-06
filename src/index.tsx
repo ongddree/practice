c48fb4b9e37c8952040196d1eaf960785b5cdc24
ongddree <ongddree@gmail.com>
2022-05-06 22:59:53 +0900

    ✨ RecoilRoot 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { RecoilRoot } from 'recoil';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
